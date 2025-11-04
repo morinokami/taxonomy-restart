22eea16a0d229768e774e46df215b21f3edaf532
Shinya Fujino <shf0811@gmail.com>
2025-11-04 22:21:20 +0900

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+const config = defineConfig({
+	plugins: [
+		devtools(),
+		tailwindcss(),
+		tanstackStart(),
+		viteReact({
+			babel: {
+				plugins: ["babel-plugin-react-compiler"],
+			},
+		}),
+	],
+});
+
+export default config;
