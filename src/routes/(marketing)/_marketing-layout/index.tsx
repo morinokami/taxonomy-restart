f5ea80be9b42e6999127713b61c5c4eac5c05c63
Shinya Fujino <shf0811@gmail.com>
2025-11-10 21:28:07 +0900

    remove data folder

--- src/routes/(marketing)/_marketing-layout/index.tsx
+++ src/routes/(marketing)/_marketing-layout/index.tsx
@@ -2,7 +2,7 @@ import { createFileRoute, Link } from "@tanstack/react-router";
 
 import { buttonVariants } from "@/components/ui/button";
 import { siteConfig } from "@/config/site";
-import { getGithubStars } from "@/data/github";
+import { getGithubStars } from "@/lib/github";
 import { cn } from "@/lib/utils";
 
 export const Route = createFileRoute("/(marketing)/_marketing-layout/")({
